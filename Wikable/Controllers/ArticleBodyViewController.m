5a910d7c26d71da5b58693d741bdd93e64f477ee
John Hearn <jhearn@gmail.com>
2016-12-19 13:37:03 -0800

    basic dynamic text function and text-to-speech seem to be working

--- Wikable/Controllers/ArticleBodyViewController.m
+++ Wikable/Controllers/ArticleBodyViewController.m
@@ -10,8 +10,8 @@
 #import "LoremIpsum.h"
 
 @interface ArticleBodyViewController ()
-//@property(strong, nonatomic)UIView * mainView;
-
+@property (weak, nonatomic) IBOutlet UIScrollView *scrollView;
+@property (weak, nonatomic) IBOutlet UIView *scrollSubView;
 @property (weak, nonatomic) IBOutlet UILabel *bodyLabel;
 
 @end
@@ -20,14 +20,43 @@
 
 
 
-- (void)viewDidLoad {
+-(void)viewDidLoad {
     [super viewDidLoad];
+    [[NSNotificationCenter defaultCenter] addObserver:self
+                                             selector:@selector(didChangePreferredContentSize:)
+                                                 name:UIContentSizeCategoryDidChangeNotification
+                                               object:nil];
 
     self.bodyLabel.text = kLoremIpsum;
 
+    UIFont *myFont = [UIFont preferredFontForTextStyle:UIFontTextStyleBody];
+
+
+    
 
 }
 
+- (void)viewDidAppear:(BOOL)animated
+{
+    [super viewDidAppear:animated];
+    [self configureView];
+}
 
+- (void)didChangePreferredContentSize:(NSNotification *)notification
+{
+    [self configureView];
+}
+
+
+- (void)configureView
+{
+    NSLog(@"%@", [[UIApplication sharedApplication] preferredContentSizeCategory] );
+    self.bodyLabel.font = [UIFont preferredFontForTextStyle:UIFontTextStyleBody];
+}
+
+-(void)dealloc
+{
+    [[NSNotificationCenter defaultCenter] removeObserver:self];
+}
 
 @end
