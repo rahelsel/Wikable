95d2841bfeed0182514511d54f8f5bbcff3439ef
John Hearn <jhearn@gmail.com>
2016-12-19 08:14:32 -0800

    initial commit

---
+++ Wikable/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Wikable
+//
+//  Created by John D Hearn on 12/19/16.
+//  Copyright © 2016 Bastardized Productions. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Wikable/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Wikable
+//
+//  Created by John D Hearn on 12/19/16.
+//  Copyright © 2016 Bastardized Productions. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
