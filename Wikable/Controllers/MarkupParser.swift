aa73ee25d2b4a4b70b1f70de119db3d63a44e3b2
John Hearn <jhearn@gmail.com>
2016-12-21 16:59:56 -0800

    partial implementation converting plaintext to NSAttributedStrings

--- Wikable/Controllers/MarkupParser.swift
+++ Wikable/Controllers/MarkupParser.swift
@@ -6,7 +6,7 @@
 //  Copyright © 2016 Bastardized Productions. All rights reserved.
 //
 
-import Foundation
+import UIKit
 
 // This has to live outside the class for it to be available to a computed property,
 // which we need for use of our singleton with bridging headers
@@ -30,30 +30,52 @@ class MarkupParser: NSObject {
     private class func getPlaintextAndMarkupFor(_ title: String) {
         WikipediaAPI.getArticleFor( title) { (plaintext) in
             WikipediaAPI.getRawMarkup(for: title) { (markup) in
-                MarkupParser.linkify(plaintext, markup)
+                var decoratedLines = MarkupParser.decorate(plaintext)
             }
         }
     }
 
-    private class func linkify(_ plaintext: String, _ markup: String) {
-        // We're going to ignore lines that begin with:
-        //   {, }, <, >, |, *, or whitespace
-        let pattern = "^[\\{\\}\\<\\>\\|\\*\\s]"
-        let regex = try! NSRegularExpression(pattern: pattern, options: [])
-        var wasFound = false
+    private class func decorate(_ plaintext: String) -> [NSAttributedString] {
+        let bodyFont = UIFont.preferredFont(forTextStyle: .body)
+        let headlineFont = UIFont.preferredFont(forTextStyle: .headline)
+        //var lines = plaintext.components(separatedBy: .newlines)
 
-        var markupLines = markup.components(separatedBy: .newlines)
-
-//        for line in markupLines {
-//            regex.enumerateMatches(in: line,
-//                                   options: [],
-//                                   range: NSRange(location: 0,
-//                                   length: line.characters.count)) {
-//                (match, _, stop) in
-//            }
-//        }
-
-        var plaintextLines: [String] = plaintext.components(separatedBy: .newlines)
-
+        return plaintext.components(separatedBy: .newlines).flatMap{
+            if MarkupParser.isHeading($0) {
+                return NSAttributedString.init(string: $0,
+                                               attributes: [NSFontAttributeName: headlineFont])
+            } else {
+                return NSAttributedString.init(string: $0,
+                                               attributes: [NSFontAttributeName: bodyFont])
+            }
+        }
     }
+
+    private class func isHeading(_ line: String) -> Bool {
+
+        return false
+    }
+
+
+//    private class func linkify(_ plaintext: String, _ markup: String) {
+//        // We're going to ignore lines that begin with:
+//        //   {, }, <, >, |, *, or whitespace
+//        let pattern = "^[\\{\\}\\<\\>\\|\\*\\s]"
+//        let regex = try! NSRegularExpression(pattern: pattern, options: [])
+//        var wasFound = false
+//
+//        var markupLines = markup.components(separatedBy: .newlines)
+//
+////        for line in markupLines {
+////            regex.enumerateMatches(in: line,
+////                                   options: [],
+////                                   range: NSRange(location: 0,
+////                                   length: line.characters.count)) {
+////                (match, _, stop) in
+////            }
+////        }
+//
+//        var plaintextLines: [String] = plaintext.components(separatedBy: .newlines)
+//
+//    }
 }
