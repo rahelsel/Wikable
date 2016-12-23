d5661d504cc85438941e19d0e6db74e1cae2ee53
Rae Helsel <rahelsel@me.com>
2016-12-22 17:40:27 -0800

    wikiAPI test

---
+++ ArticleBodyViewControllerTests/WikipediaAPITest.m
@@ -0,0 +1,44 @@
+//
+//  WikipediaAPITest.m
+//  Wikable
+//
+//  Created by Rachael A Helsel on 12/22/16.
+//  Copyright © 2016 Bastardized Productions. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+#import "WikipediaAPI.h"
+
+@interface WikipediaAPITest : XCTestCase
+
+@property(nonatomic) WikipediaAPI *wikipediaAPItest;
+
+@end
+
+@implementation WikipediaAPITest
+
+- (void)setUp {
+    [super setUp];
+    
+    self.wikipediaAPItest = [[WikipediaAPI alloc]init];
+    
+}
+
+- (void)tearDown {
+        [super tearDown];
+}
+
+-(void)testURLfrom{
+    
+    NSString *testString1 = @"http://www.preeminent.org/steve/iOSTutorials/XCTest/";
+    NSString *testString2 = @" Siamese Cat ";
+    NSURL *testNSURL = [WikipediaAPI urlFrom:testString1 and:testString2];
+    
+    NSLog(@"%@", testNSURL.absoluteString);
+    
+    XCTAssertTrue(![testNSURL.absoluteString containsString:@" "], @"Still contains space character.");
+}
+
+
+
+@end
